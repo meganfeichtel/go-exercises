26eaafd4d4208a3175deb122e885db671b669611
meganfeichtel <mfeichtel@redventures.com>
2019-07-26 15:41:57 -0400

    completed the aaah problem in Kattis

---
+++ kattis-submissions/aaah.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+)
+
+//Aaah! Problem
+//Link: https://open.kattis.com/problems/aaah
+//Difficulty Level: 1.6
+//ACCEPTED
+func main() {
+	var a, b string
+	var res string
+	fmt.Scanf("%s", &a)
+	fmt.Scanf("%s", &b)
+
+	if len(a) >= len(b) {
+		res = "go"
+	} else {
+		res = "no"
+	}
+
+	fmt.Printf("%s\n", res)
+}
